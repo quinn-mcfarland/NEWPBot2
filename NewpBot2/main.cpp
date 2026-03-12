09fc296e9217766e024f70ac75888be686333738
Quinn McFarland <lilmcfarlands@gmail.com>
2026-03-11 21:45:27 -0700

    Created VS Solution
    Created the solution files VS needs me to develop the file, as well as adding some usually universal includes into main.cpp and a main function.

---
+++ NewpBot2/main.cpp
@@ -0,0 +1,18 @@
+// Library Includes
+#include <cstdlib>
+
+// Header Includes
+
+// Constant Declarations
+
+// Variable Declarations
+
+// Function Prototypes
+
+// Main Function
+int main()
+{
+	return 0;
+}
+
+// Function Definitions
