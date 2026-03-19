4ed5653d7593cd3c686f0c3a1ea8cfc1e0266dad
Quinn McFarland <lilmcfarlands@gmail.com>
2026-03-18 17:23:13 -0700

    Rewrote main source file
    Rewrote the main source file and function for the bot one last time.

--- MyBot/main.cpp
+++ MyBot/main.cpp
@@ -3,10 +3,7 @@
 
 // Header Inclusions
 #include"MyBot.h"
-#include"bgsStarSystem.h"
-#include"powerplayStarSystem.h"
 #include"cmdrClass.h"
-#include"monthlyLeaderboard.h"
 
 // Global Constants
 const std::string BOT_TOKEN = "MTQ4MTQ2NzY4NDEyMDk1MzAzNQ.GsvzZn.kr-GlGNkiXCuPSMuqEIEaFhkKgbbzKjk8nuZ5A";
@@ -18,6 +15,26 @@ const dpp::snowflake MY_GUILD_ID = 529089208254464001;
 
 // Main Function
 int main() {
+	// Create a new bot cluster
+	dpp::cluster bot(BOT_TOKEN);
+
+	// Output log information to console
+	bot.on_log(dpp::utility::cout_logger());
+
+	// Handle slash commands
+	bot.on_slashcommand([](const dpp::slashcommand_t& event) {
+		// Check which command was used
+	});
+
+	// Register bot commands on ready
+	bot.on_ready([&bot](const dpp::ready_t& event) {
+		if (dpp::run_once<struct register_bot_commands>()) {
+			// Create commands here
+		}
+	});
+
+	// Start the bot
+	bot.start(dpp::st_wait);
 	
 	return 0;
 }
