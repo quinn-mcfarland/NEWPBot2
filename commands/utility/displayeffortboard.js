5912d7a2c327081d3639ec510cb3cdff74fca004
Quinn McFarland <lilmcfarlands@gmail.com>
2026-04-04 16:05:36 -0700

    Replaced the ASCII Table dependency from ASCII-Tables 3 to visulima/tabular

--- commands/utility/displayeffortboard.js
+++ commands/utility/displayeffortboard.js
@@ -1,16 +1,18 @@
 // Module Imports
 const { SlashCommandBuilder, TextDisplayBuilder, MessageFlags } = require('discord.js');
-const { activeBgsSystems, activePowerplaySystems } = require('/Users/quinnmcfarland/Repos/NEWPBot2/effortdata.js');
+const { activeBgsSystems, activePowerplaySystems, BGSSystem } = require('/Users/quinnmcfarland/Repos/NEWPBot2/effortdata.js');
 
 // Variables
-let bgsDisplay;
-let powerplayDisplay;
 
 // Helper Functions
-async function displayTableBuilder(startingArray, endingString) {
+async function displayTableBuilder(startingArray, tableType) {
 
 }
 
 // Text Display Component
 
 // Command Execution
+activeBgsSystems.push(new BGSSystem('test1'));
+activeBgsSystems.push(new BGSSystem('test2'));
+activeBgsSystems.push(new BGSSystem('test3'));
+displayTableBuilder(activeBgsSystems, 'BGS');
