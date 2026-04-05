3df65282394a1a5fc9d7b4bc203d40b0bc3c04e7
Quinn McFarland <lilmcfarlands@gmail.com>
2026-04-05 11:59:27 -0700

    Added some utility scripts to the deploy/undeploy pipeline

---
+++ deleteCommands.js
@@ -0,0 +1,16 @@
+const { REST, Routes } = require('discord.js');
+const { clientId, guildId, token } = require('./config.json');
+
+const rest = new REST().setToken(token);
+
+// for guild based commands
+rest
+    .delete(Routes.applicationGuildCommand(clientId, guildId, 'commandId'))
+    .then(() => console.log('Successfully deleted guild command'))
+    .catch(console.error);
+
+// for global commands
+rest
+    .delete(Routes.applicationCommands(clientId, 'commandId'))
+    .then(() => console.log('Successfully deleted application command'))
+    .catch(console.error);
